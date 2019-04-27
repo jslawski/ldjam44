a2fb297e62935d0661ebf2d1fd846c6dc49b9a8b
Azim Momin <azimmomin6@gmail.com>
2019-04-27 12:30:16 -0700

    Initial sequence retriever functionality.

--- Assets/Scripts/SequenceRetriever.cs
+++ Assets/Scripts/SequenceRetriever.cs
@@ -1,12 +1,44 @@
-﻿/**
- * This class retrieves a sequence scriptable object from
- * our assets.
- */
+﻿using UnityEngine;
+
+/**
+* This class retrieves a sequence scriptable object from
+* our assets.
+*/
 public static class SequenceRetriever
 {
-  // TODO: Implement this and have it return the correct type.
-  public static void GetNextSequence(GameDifficulty gameDifficulty)
+  
+  /// <param name="gameDifficulty">The difficulty the player is playing at.</param>
+  /// <param name="previousSequence">The sequence the player just played at. We don't want to return the same sequence.</param>
+  /// <returns>A sequence for the player to play or null if we can't find one.</returns>
+  public static Sequence GetNextSequence(GameDifficulty gameDifficulty, Sequence previousSequence)
   {
+    Object[] loadedSequencesForDifficulty = GetSequenceAssetsForGameDifficulty(gameDifficulty);
+    Sequence sequence = PickSequence(loadedSequencesForDifficulty, previousSequence);
+    if (sequence == null) { return null; }
+    return sequence;
+  }
 
+  /// <summary>
+  /// We've divided our sequences by difficulty. This function gets all sequences
+  /// for a given difficulty.
+  /// </summary>
+  /// <param name="gameDifficulty">The difficulty the player is playing at.</param>
+  /// <returns>An array of all sequences, as objects, for the given difficulty level.</returns>
+  private static Object[] GetSequenceAssetsForGameDifficulty(GameDifficulty gameDifficulty)
+  {
+    string assetPath = "Assets/Resources/Sequences/" + gameDifficulty.ToString();
+    return Resources.LoadAll(assetPath);
+  }
+
+  /// <summary>
+  /// Picks a random sequence out of the array of loaded sequences while
+  /// making sure that we don't reload the sequence the user just played at.
+  /// </summary>
+  /// <param name="loadedSequences">All the eligible sequences to choose from.</param>
+  /// <param name="previousSequence">The sequence the player just played.</param>
+  /// <returns></returns>
+  private static Sequence PickSequence(Object[] loadedSequences, Sequence previousSequence)
+  {
+    return null;
   }
 }
