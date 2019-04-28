9a9f509b5e391be7f7d3e239b7edd32f90b1bdcf
jslawski <jslawski@umich.edu>
2019-04-28 15:52:41 -0400

    Cutscene manager

---
+++ Assets/Scripts/VideoManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoManager : MonoBehaviour {
+
+  [SerializeField]
+  private VideoPlayer player;
+  [SerializeField]
+  private string videoName; //Include Extension
+
+	void Awake() {
+    player.url = System.IO.Path.Combine(Application.streamingAssetsPath, this.videoName);
+    player.Play();
+
+    player.loopPointReached += this.LoadGameScene;
+	}
+
+  public void SkipCutscene()
+  {
+    this.player.Stop();
+    this.LoadGameScene(this.player);
+  }
+
+  private void LoadGameScene(VideoPlayer source)
+  {
+    SceneManager.LoadScene("main");
+  }
+}
