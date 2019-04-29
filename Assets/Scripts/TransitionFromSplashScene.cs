b38a734caddcca69763a1f8f4c592b5d203597bb
Azim Momin <azimmomin6@gmail.com>
2019-04-28 22:26:30 -0700

    Adding splash scene and title scene with transitions. Also, deleted the old granny cut scene.

---
+++ Assets/Scripts/TransitionFromSplashScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionFromSplashScene : MonoBehaviour
+{
+  [SerializeField]
+  private float secondsToWaitBeforeTransition = 3.0f;
+  [SerializeField]
+  private string sceneToTransitionTo = string.Empty;
+
+  private void Awake()
+  {
+    StartCoroutine(TransitionAfterWait());
+  }
+
+  private IEnumerator TransitionAfterWait()
+  {
+    yield return new WaitForSeconds(secondsToWaitBeforeTransition);
+
+    if (sceneToTransitionTo != string.Empty)
+    {
+      SceneManager.LoadScene(sceneToTransitionTo);
+    }
+
+  }
+}
---
+++ Assets/Scripts/TransitionFromTitleScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class TransitionFromTitleScene : MonoBehaviour, IPointerClickHandler
+{
+  [SerializeField]
+  private string sceneToTransitionTo = string.Empty;
+
+  public void OnPointerClick(PointerEventData eventData)
+  {
+    if (sceneToTransitionTo == string.Empty)
+    {
+      return;
+    }
+
+    SceneManager.LoadScene(sceneToTransitionTo);
+  }
+}
