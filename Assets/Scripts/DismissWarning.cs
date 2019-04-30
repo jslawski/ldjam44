e91a954cb9c60fef14074aa1ba3e076a1c672e13
jslawski <jslawski@umich.edu>
2019-04-29 19:05:09 -0700

    Making WebGL playable

---
+++ Assets/Scripts/DismissWarning.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DismissWarning : MonoBehaviour {
+
+	// Use this for initialization
+	public void DismissWebGLWarning() {
+        SceneManager.LoadScene("TitleScene");
+    }
+}
